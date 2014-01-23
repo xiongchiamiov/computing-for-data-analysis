bb9978506dafc643a5306715f305fc2fa16dfe8e
xiongchiamiov <xiong.chiamiov@gmail.com>
2014-01-23 01:06:15 -0800

    Part 5: first attempt
    This seems roughly right, but while I get correct results for heart attacks, I
    don't match the example output for heart failure or pneumonia.

---
+++ assignment-3/best.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+############
+# Find the hospital in a state with the lowest mortality rate for an issue.
+#
+#   state: The two-character abbreviation for a state.
+# outcome: Why the patient came in.  One of: 'heart attack', 'heart failure',
+#          'pneumonia'.
+best = function(state, outcome) {
+	# Rename to make more sense than what the spec requires.
+	condition = outcome
+	
+	outcomes = read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	
+	# Filter by state.
+	stateFilter = outcomes[['State']] == state
+	outcomes = outcomes[stateFilter,]
+	if (nrow(outcomes) == 0) {
+		# It would be nice to include *what* the state is, but the spec says
+		# otherwise...
+		stop('invalid state')
+	}
+	
+	# Sort by condition.  Magic numbers aplenty.
+	# R's switch statement is actually a function, and therefore totally
+	# inappropriate for this.
+	if (condition == 'heart attack') {
+		orderingColumn = 11
+	} else if (condition == 'heart failure') {
+		orderingColumn = 17
+	} else if (condition == 'pneumonia') {
+		orderingColumn = 23
+	} else {
+		stop('invalid outcome')
+	}
+	outcomes = outcomes[order(outcomes[, orderingColumn]),]
+	bestOutcome = head(outcomes, n=1)
+	
+	return(bestOutcome[['Hospital.Name']])
+}
+
